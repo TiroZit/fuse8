28c8c0da4453cc837d802a3f1a8046ecfa00902d
TiroZit <nikus.master@gmail.com>
2022-04-06 18:17:10 +0500

    scss globally

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,5 @@ export default {
 </script>
 
 <style lang='scss'>
-header {
-  font-size: rem(20);
-}
+
 </style>
--- src/components/UI/AsideBar.vue
+++ src/components/UI/AsideBar.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
 aside
-  |cooool
 </template>
 <script>
 export default {
@@ -8,7 +7,5 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-aside{
-  font-size: rem(20);
-}
+
 </style>
