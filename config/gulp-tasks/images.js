fe57dbc85c73f8eea7f092df730f69bc0c4897b9
Avdeev Nikita <78494774+TiroZit@users.noreply.github.com>
2022-04-08 10:04:38 +0500

    Update images.js

--- config/gulp-tasks/images.js
+++ config/gulp-tasks/images.js
@@ -36,12 +36,6 @@ export const images = () => {
 				app.gulp.src(app.path.src.images)
 			)
 		)
-		.pipe(
-			app.plugins.if(
-				app.isWebP,
-				app.plugins.newer(app.path.build.images)
-			)
-		)
 		.pipe(app.gulp.src(app.path.src.svg))
 		.pipe(app.gulp.dest(app.path.build.images));
 }
