1be2af131d47a392c1da8318d4f7b87533cc39af
TiroZit <nikus.master@gmail.com>
2022-04-11 19:04:08 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 (function() {
   var __webpack_modules__ = {
     745: function() {},
-    191: function() {},
+    872: function() {},
+    899: function() {},
+    857: function() {},
+    6: function() {},
+    705: function() {},
     311: function() {},
     261: function() {},
     895: function() {},
@@ -16,13 +20,49 @@
       add("63ddc780", content, false, {});
       if (false) ;
     },
-    883: function(module, __unused_webpack_exports, __webpack_require__) {
-      var content = __webpack_require__(191);
+    452: function(module, __unused_webpack_exports, __webpack_require__) {
+      var content = __webpack_require__(872);
       if (content.__esModule) content = content.default;
       if ("string" === typeof content) content = [ [ module.id, content, "" ] ];
       if (content.locals) module.exports = content.locals;
       var add = __webpack_require__(23).Z;
-      add("4d1b3d22", content, false, {});
+      add("0a116f46", content, false, {});
+      if (false) ;
+    },
+    548: function(module, __unused_webpack_exports, __webpack_require__) {
+      var content = __webpack_require__(899);
+      if (content.__esModule) content = content.default;
+      if ("string" === typeof content) content = [ [ module.id, content, "" ] ];
+      if (content.locals) module.exports = content.locals;
+      var add = __webpack_require__(23).Z;
+      add("1c0fb885", content, false, {});
+      if (false) ;
+    },
+    241: function(module, __unused_webpack_exports, __webpack_require__) {
+      var content = __webpack_require__(857);
+      if (content.__esModule) content = content.default;
+      if ("string" === typeof content) content = [ [ module.id, content, "" ] ];
+      if (content.locals) module.exports = content.locals;
+      var add = __webpack_require__(23).Z;
+      add("1ca513e6", content, false, {});
+      if (false) ;
+    },
+    637: function(module, __unused_webpack_exports, __webpack_require__) {
+      var content = __webpack_require__(6);
+      if (content.__esModule) content = content.default;
+      if ("string" === typeof content) content = [ [ module.id, content, "" ] ];
+      if (content.locals) module.exports = content.locals;
+      var add = __webpack_require__(23).Z;
+      add("113d7826", content, false, {});
+      if (false) ;
+    },
+    995: function(module, __unused_webpack_exports, __webpack_require__) {
+      var content = __webpack_require__(705);
+      if (content.__esModule) content = content.default;
+      if ("string" === typeof content) content = [ [ module.id, content, "" ] ];
+      if (content.locals) module.exports = content.locals;
+      var add = __webpack_require__(23).Z;
+      add("a585710c", content, false, {});
       if (false) ;
     },
     424: function(module, __unused_webpack_exports, __webpack_require__) {
@@ -52,7 +92,7 @@
       add("646a3efa", content, false, {});
       if (false) ;
     },
-    899: function(module, __unused_webpack_exports, __webpack_require__) {
+    142: function(module, __unused_webpack_exports, __webpack_require__) {
       var content = __webpack_require__(178);
       if (content.__esModule) content = content.default;
       if ("string" === typeof content) content = [ [ module.id, content, "" ] ];
@@ -70,7 +110,7 @@
       add("7cc39c71", content, false, {});
       if (false) ;
     },
-    170: function(__unused_webpack___webpack_module__, __unused_webpack___webpack_exports__, __webpack_require__) {
+    925: function(__unused_webpack___webpack_module__, __unused_webpack___webpack_exports__, __webpack_require__) {
       "use strict";
       __webpack_require__(372);
       __webpack_require__(944);
@@ -85,28 +125,116 @@
         var _component_aside_bar = (0, runtime_core_esm_bundler.up)("aside-bar");
         var _component_about_person = (0, runtime_core_esm_bundler.up)("about-person");
         var _component_about_me = (0, runtime_core_esm_bundler.up)("about-me");
+        var _component_skills = (0, runtime_core_esm_bundler.up)("skills");
         return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)(runtime_core_esm_bundler.HY, null, [ _hoisted_1, (0, 
         runtime_core_esm_bundler.Wm)(_component_aside_bar), (0, runtime_core_esm_bundler._)("main", _hoisted_2, [ (0, 
-        runtime_core_esm_bundler.Wm)(_component_about_person), (0, runtime_core_esm_bundler.Wm)(_component_about_me) ]) ], 64);
+        runtime_core_esm_bundler.Wm)(_component_about_person), (0, runtime_core_esm_bundler.Wm)(_component_about_me), (0, 
+        runtime_core_esm_bundler.Wm)(_component_skills) ]) ], 64);
       }
+      var _withScopeId = function _withScopeId(n) {
+        return (0, runtime_core_esm_bundler.dD)("data-v-782ef64b"), n = n(), (0, runtime_core_esm_bundler.Cn)(), 
+        n;
+      };
+      var Skillsvue_type_template_id_782ef64b_scoped_true_lang_pug_hoisted_1 = {
+        class: "skills"
+      };
+      var Skillsvue_type_template_id_782ef64b_scoped_true_lang_pug_hoisted_2 = {
+        class: "skills__container"
+      };
+      var _hoisted_3 = _withScopeId((function() {
+        return (0, runtime_core_esm_bundler._)("h2", {
+          class: "skills__title"
+        }, "Навыки", -1);
+      }));
+      function Skillsvue_type_template_id_782ef64b_scoped_true_lang_pug_render(_ctx, _cache, $props, $setup, $data, $options) {
+        var _component_basic_skills = (0, runtime_core_esm_bundler.up)("basic-skills");
+        var _component_secondary_skills = (0, runtime_core_esm_bundler.up)("secondary-skills");
+        var _component_qualification_skills = (0, runtime_core_esm_bundler.up)("qualification-skills");
+        return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)("section", Skillsvue_type_template_id_782ef64b_scoped_true_lang_pug_hoisted_1, [ (0, 
+        runtime_core_esm_bundler._)("div", Skillsvue_type_template_id_782ef64b_scoped_true_lang_pug_hoisted_2, [ _hoisted_3, (0, 
+        runtime_core_esm_bundler.Wm)(_component_basic_skills, {
+          class: "skills__basic-skills"
+        }), (0, runtime_core_esm_bundler.Wm)(_component_secondary_skills, {
+          class: "skills__secondary-skills"
+        }), (0, runtime_core_esm_bundler.Wm)(_component_qualification_skills, {
+          class: "skills__qualification-skills"
+        }) ]) ]);
+      }
+      var BasicSkillsvue_type_template_id_1de8bce6_lang_pug_hoisted_1 = {
+        class: "basic-skills"
+      };
+      var BasicSkillsvue_type_template_id_1de8bce6_lang_pug_hoisted_2 = (0, runtime_core_esm_bundler.uE)('<h3 class="basic-skills__title">Основные навыки</h3><div class="basic-skills__items"><div class="basic-skills__item">Креативное мышление</div><div class="basic-skills__item">Web-design</div><div class="basic-skills__item">UX-design</div><div class="basic-skills__item">Фигменный стиль</div></div>', 2);
+      var _hoisted_4 = [ BasicSkillsvue_type_template_id_1de8bce6_lang_pug_hoisted_2 ];
+      function BasicSkillsvue_type_template_id_1de8bce6_lang_pug_render(_ctx, _cache, $props, $setup, $data, $options) {
+        return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)("section", BasicSkillsvue_type_template_id_1de8bce6_lang_pug_hoisted_1, _hoisted_4);
+      }
+      var BasicSkillsvue_type_script_lang_js = {
+        name: "basic-skills"
+      };
+      __webpack_require__(548);
+      var exportHelper = __webpack_require__(21);
+      const __exports__ = (0, exportHelper.Z)(BasicSkillsvue_type_script_lang_js, [ [ "render", BasicSkillsvue_type_template_id_1de8bce6_lang_pug_render ] ]);
+      var BasicSkills = __exports__;
+      var SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_hoisted_1 = {
+        class: "secondary-skills"
+      };
+      var SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_hoisted_2 = (0, 
+      runtime_core_esm_bundler.uE)('<h3 class="secondary-skills__title" data-v-ca5959ca>Вторичные навыки</h3><div class="secondary-skills__items" data-v-ca5959ca><div class="secondary-skills__item" data-v-ca5959ca>Арт-дизайн</div><div class="secondary-skills__item" data-v-ca5959ca>CSS</div><div class="secondary-skills__item" data-v-ca5959ca>Брендинг</div><div class="secondary-skills__item" data-v-ca5959ca>Графический дизайнер</div><div class="secondary-skills__item" data-v-ca5959ca>HTML</div><div class="secondary-skills__item" data-v-ca5959ca>Социальные сети</div><div class="secondary-skills__item" data-v-ca5959ca>Иллюстрации</div></div>', 2);
+      var SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_hoisted_4 = [ SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_hoisted_2 ];
+      function SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_render(_ctx, _cache, $props, $setup, $data, $options) {
+        return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)("section", SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_hoisted_1, SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_hoisted_4);
+      }
+      var SecondarySkillsvue_type_script_lang_js = {
+        name: "secondary-skills"
+      };
+      __webpack_require__(637);
+      const SecondarySkills_exports_ = (0, exportHelper.Z)(SecondarySkillsvue_type_script_lang_js, [ [ "render", SecondarySkillsvue_type_template_id_ca5959ca_scoped_true_lang_pug_render ], [ "__scopeId", "data-v-ca5959ca" ] ]);
+      var SecondarySkills = SecondarySkills_exports_;
+      var QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_hoisted_1 = {
+        class: "qualification-skills"
+      };
+      var QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_hoisted_2 = (0, 
+      runtime_core_esm_bundler.uE)('<h3 class="qualification-skills__title" data-v-a456cf1e>Квалификация </h3><div class="qualification-skills__items" data-v-a456cf1e><div class="qualification-skills__item" data-v-a456cf1e><span data-v-a456cf1e>Курсы Skillbox</span><span data-v-a456cf1e><time class="qualification-skills__time qualification-skills__time_begin" datetime="2021" data-v-a456cf1e>2021</time><time class="qualification-skills__time qualification-skills__time_end" datetime="2022" data-v-a456cf1e>2022</time></span></div><div class="qualification-skills__item" data-v-a456cf1e><span data-v-a456cf1e>Курсы GeekBrains</span><span data-v-a456cf1e><time class="qualification-skills__time qualification-skills__time_begin" datetime="2019" data-v-a456cf1e>2019</time><time class="qualification-skills__time qualification-skills__time_end" datetime="2020" data-v-a456cf1e>2020</time></span></div></div>', 2);
+      var QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_hoisted_4 = [ QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_hoisted_2 ];
+      function QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_render(_ctx, _cache, $props, $setup, $data, $options) {
+        return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)("section", QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_hoisted_1, QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_hoisted_4);
+      }
+      var QualificationSkillsvue_type_script_lang_js = {
+        name: "qualification-skills"
+      };
+      __webpack_require__(241);
+      const QualificationSkills_exports_ = (0, exportHelper.Z)(QualificationSkillsvue_type_script_lang_js, [ [ "render", QualificationSkillsvue_type_template_id_a456cf1e_scoped_true_lang_pug_render ], [ "__scopeId", "data-v-a456cf1e" ] ]);
+      var QualificationSkills = QualificationSkills_exports_;
+      var Skillsvue_type_script_lang_js = {
+        name: "skills",
+        components: {
+          BasicSkills: BasicSkills,
+          SecondarySkills: SecondarySkills,
+          QualificationSkills: QualificationSkills
+        }
+      };
+      __webpack_require__(995);
+      const Skills_exports_ = (0, exportHelper.Z)(Skillsvue_type_script_lang_js, [ [ "render", Skillsvue_type_template_id_782ef64b_scoped_true_lang_pug_render ], [ "__scopeId", "data-v-782ef64b" ] ]);
+      var Skills = Skills_exports_;
       var Appvue_type_script_lang_js = {
-        components: {},
+        components: {
+          Skills: Skills
+        },
         data: function data() {
           return {};
         },
         methods: {}
       };
-      __webpack_require__(883);
-      var exportHelper = __webpack_require__(21);
-      const __exports__ = (0, exportHelper.Z)(Appvue_type_script_lang_js, [ [ "render", render ] ]);
-      var App = __exports__;
+      __webpack_require__(452);
+      const App_exports_ = (0, exportHelper.Z)(Appvue_type_script_lang_js, [ [ "render", render ] ]);
+      var App = App_exports_;
       var AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_1 = {
         class: "aside"
       };
       var AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_2 = (0, runtime_core_esm_bundler.uE)('<div class="aside__wrapper" data-v-257f4490><div class="aside__logo" data-v-257f4490><svg class="i-logo" aria-hidden data-v-257f4490><use xlink:href="img/icons/icons.svg#svg-logo" data-v-257f4490></use></svg></div><div class="aside__menu" data-v-257f4490><div class="aside__burger" data-v-257f4490><button class="burger" type="button" data-v-257f4490><span data-v-257f4490></span></button></div><a class="aside__search" href="#" data-v-257f4490><svg class="i-search" style="fill:currentColor;" aria-hidden data-v-257f4490><use xlink:href="img/icons/icons.svg#svg-search" data-v-257f4490></use></svg></a><a class="aside__edit active" href="#" data-v-257f4490><svg class="i-pencil" style="fill:currentColor;" aria-hidden data-v-257f4490><use xlink:href="img/icons/icons.svg#svg-pencil" data-v-257f4490></use></svg></a><a class="aside__avatar" href="#" data-v-257f4490><picture data-v-257f4490><source srcset="img/avatar.avif" type="image/avif" data-v-257f4490><source srcset="img/avatar.webp" type="image/webp" data-v-257f4490><img src="img/avatar.jpg" alt="аватар" width="41" height="41" loading="lazy" data-v-257f4490></picture></a></div></div>', 1);
-      var _hoisted_3 = [ AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_2 ];
+      var AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_3 = [ AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_2 ];
       function AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_render(_ctx, _cache, $props, $setup, $data, $options) {
-        return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)("aside", AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_1, _hoisted_3);
+        return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)("aside", AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_1, AsideBarvue_type_template_id_257f4490_scoped_true_lang_pug_hoisted_3);
       }
       var AsideBarvue_type_script_lang_js = {
         name: "aside-bar"
@@ -127,7 +255,7 @@
       var AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_hoisted_3 = {
         class: "about-person__wrapper"
       };
-      var _hoisted_4 = AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_withScopeId((function() {
+      var AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_hoisted_4 = AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_withScopeId((function() {
         return (0, runtime_core_esm_bundler._)("div", {
           class: "about-person__avatar"
         }, [ (0, runtime_core_esm_bundler._)("picture", null, [ (0, runtime_core_esm_bundler._)("source", {
@@ -153,7 +281,7 @@
         var _component_personal_facts = (0, runtime_core_esm_bundler.up)("personal-facts");
         return (0, runtime_core_esm_bundler.wg)(), (0, runtime_core_esm_bundler.iD)("section", AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_hoisted_1, [ (0, 
         runtime_core_esm_bundler._)("div", AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_hoisted_2, [ (0, 
-        runtime_core_esm_bundler._)("div", AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_hoisted_3, [ _hoisted_4, (0, 
+        runtime_core_esm_bundler._)("div", AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_hoisted_3, [ AboutPersonvue_type_template_id_423ef2e2_scoped_true_lang_pug_hoisted_4, (0, 
         runtime_core_esm_bundler._)("div", _hoisted_5, [ _hoisted_6, (0, runtime_core_esm_bundler.Wm)(_component_socials, {
           class: "about-person__socials"
         }) ]) ]), (0, runtime_core_esm_bundler.Wm)(_component_personal_facts, {
@@ -192,7 +320,7 @@
       var PersonalFactsvue_type_script_lang_js = {
         name: "personal-facts"
       };
-      __webpack_require__(899);
+      __webpack_require__(142);
       const PersonalFacts_exports_ = (0, exportHelper.Z)(PersonalFactsvue_type_script_lang_js, [ [ "render", PersonalFactsvue_type_template_id_d4361bda_scoped_true_lang_pug_render ], [ "__scopeId", "data-v-d4361bda" ] ]);
       var PersonalFacts = PersonalFacts_exports_;
       var AboutMevue_type_template_id_963e943c_scoped_true_lang_pug_hoisted_1 = {
@@ -313,7 +441,7 @@
     chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
   }();
   var __webpack_exports__ = __webpack_require__.O(void 0, [ 216 ], (function() {
-    return __webpack_require__(170);
+    return __webpack_require__(925);
   }));
   __webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 })();
