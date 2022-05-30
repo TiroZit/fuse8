9c59667a321df0987cc52ac974596c2e0e6e6124
TiroZit <nikus.master@gmail.com>
2022-05-30 15:54:48 +0500

    Адаптивность

--- src/components/Skills/BasicSkills.vue
+++ src/components/Skills/BasicSkills.vue
@@ -8,25 +8,26 @@ section.basic-skills
 </template>
 <script>
 export default {
-  name: "basic-skills",
+  name: 'basic-skills',
   props: {
     skillsBase: {},
   },
-};
+}
 </script>
 <style lang="scss">
 .basic-skills {
   &__title,
   &__item {
     font-weight: 700;
-    @include adaptiveValue("font-size", 28, 18);
+    @include adaptiveValue('font-size', 28, 18);
   }
   &__items {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     margin-top: rem(25);
-    gap: rem(10);
+    @include adaptiveValue('column-gap', 100, 10);
+    row-gap: rem(20);
   }
   &__item {
     position: relative;
@@ -34,25 +35,25 @@ export default {
     align-items: center;
     justify-content: center;
     flex: 0 0 rem(215);
-    @include adaptiveValue("flex-basis", 215, 140);
-    @include adaptiveValue("min-height", 215, 140);
+    @include adaptiveValue('flex-basis', 215, 140);
+    @include adaptiveValue('min-height', 215, 140);
     text-align: center;
     border-radius: 50%;
     background-color: var(--color-yellow);
-    @include media-breakpoint-up(container) {
-      &:not(:last-child) {
-        &::after {
-          content: "";
-          position: absolute;
-          top: calc(50% - rem(4));
-          right: calc(-50% - rem(23.5));
-          display: block;
-          width: rem(95);
-          height: rem(8);
-          background-color: var(--color-blackRaisin);
-        }
-      }
-    }
+    // @include media-breakpoint-up(container) {
+    //   &:not(:last-child) {
+    //     &::after {
+    //       content: '';
+    //       position: absolute;
+    //       top: calc(50% - rem(4));
+    //       right: calc(-50% - rem(23.5));
+    //       display: block;
+    //       width: rem(95);
+    //       height: rem(8);
+    //       background-color: var(--color-blackRaisin);
+    //     }
+    //   }
+    // }
   }
 }
 </style>
